5d2d67868a8bdde5e21a9ff5db998b35410f3a7d
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-03-27 10:53:13 +0700

    create screen

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("#708090")
+screen.title("Get A Grip On My Snake")
+
+screen.exitonclick()
+
+
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
